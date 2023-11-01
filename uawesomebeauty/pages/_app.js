a1313af667aa323946f404aad011c1d9b129dc3b
Aaron1357 <kimjonghwi1540@gmail.com>
2023-11-01 15:33:20 +0900

    favicon ++

---
+++ uawesomebeauty/pages/_app.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function App() {
+  return (
+    <div>
+      <Head>
+        <title>유어썸뷰티 | About Us</title>
+      </Head>
+      {/* 다른 컴포넌트나 내용을 추가할 수 있습니다. */}
+    </div>
+  );
+}
